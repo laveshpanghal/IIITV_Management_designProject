a266e4c8d675af45ebf531096e08fa3cc42551c8
TechMangalam <201951089@iiitvadodara.ac.in>
2021-08-29 00:10:35 +0530

    update

--- src/Components/Pages/Admission.js
+++ src/Components/Pages/Admission.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import "../../Login/login.css"
+
+function verify(){
+
+	var applicantEnrollmentNo = document.getElementById("enrollmentInput").value;
+	if(applicantEnrollmentNo == null){
+		alert("Enrollment no should not be blank");
+	
+	}else{
+	
+}}
 
 const Admission = () => {
 return (
-	<div className="container justify-content-center">
-	<h3 className="my-20">Enter your JEE Enrollment no. to verify</h3>
-
-	<form className="d-flex-vertical">
-	<div className="form-group ">
-		<label htmlFor="exampleInputEmail1">Email enrollment no</label><br/>
-		<input type="number" id="enrollmentInput" />
-	</div>
+	<div className="container text-center">
 	
-	<button type="submit" className="btn btn-primary">Submit</button>
-	</form>
+		<div className="card w-50 my-5 mx-auto">
+
+			<br/>
+			<br/>
+
+			<h3 className="my-20">Enter your JEE Enrollment no. to verify</h3>
+
+			<form className="d-flex-vertical my-10">
+			<div className="form-group ">
+				<br/>
+				<input className="input-field" id="enrollmentInput" />
+			</div>
+
+			<button type="submit" className="btn btn-primary my-5" onClick={verify}>Submit</button>
+			</form>
+
+		</div>
 
 	</div>
 );
--- src/Login/login.js
+++ src/Login/login.js
@@ -44,28 +44,27 @@ function Login() {
   return(
 <>
 
-<div id="container" className="container-fluid text-center">
-  <div className="row con-center">
-    <div className="col-sm-10 offset-sm-1 con-center">
+<div id="container" className="container text-center">
+  <div className="row ">
+    <div className="col-sm-10 offset-sm-1 text-center">
 
-    <div action="./" className="sign-in-form con-center">
-  <img src="https://firebasestorage.googleapis.com/v0/b/iiitv-198b6.appspot.com/o/ContentImages%2FIIITV_Logo.jpg?alt=media&token=fbcc68ab-82f5-498f-a669-0f6bd0227809" style={{height: '120px', width: '110px',margin:'80px'}} alt="" />
-  <h2 className="title">IIIT Vadodara Sign in</h2>
-  <div className="input-field">
-    <i className="fas fa-user" />
-    <input type="email" placeholder="Username" id="emailS" />
+        <div action="./" className="sign-in-form align-center">
+          <img src="https://firebasestorage.googleapis.com/v0/b/iiitv-198b6.appspot.com/o/ContentImages%2FIIITV_Logo.jpg?alt=media&token=fbcc68ab-82f5-498f-a669-0f6bd0227809" style={{height: '120px', width: '110px',margin:'80px'}} alt="" />
+          <h2 className="title">IIIT Vadodara Sign in</h2>
+          <div className="input-field">
+            <i className="fas fa-user" />
+            <input type="email" placeholder="Username" id="emailS" />
+          </div>
+          <div className="input-field">
+            <i className="fas fa-lock" />
+            <input type="password" placeholder="Password" id="passwordS" />
+          </div>
+          <input defaultValue="Login" className="btn-submit solid text-center" id="sign_in_btn" onClick={login} />
+          <div id="password_reset">
+            Forgot password ,Click to reset
+          </div>
+      </div>
   </div>
-  <div className="input-field">
-    <i className="fas fa-lock" />
-    <input type="password" placeholder="Password" id="passwordS" />
-  </div>
-  <input defaultValue="Login" className="btn solid" id="sign_in_btn" onClick={login} />
-  <div id="password_reset">
-    Forgot password ,Click to reset
-  </div>
-</div>
-
-</div>
 </div>
 </div>
 
