957a831bfc501c0540d59337feb7d3a2b8c7eb76
TechMangalam <201951089@iiitvadodara.ac.in>
2021-09-03 22:29:41 +0530

    Admin restructured

--- src/Components/Pages/admin/AdminDashboard.js
+++ src/Components/Pages/admin/AdminDashboard.js
@@ -1,45 +1,41 @@
 import React, {useState} from "react";
+import "../Styles/Admindash.css"
 
 const AdminDashboard=()=> {
 
   return (
-
       <div id="content">
-        <nav id="nav1">
-          <a href="#page1">
-            <div>
-              <img src="../Styles/user.png" alt=""/>
-              <p>Admin name</p>
-              <p> Role</p>
-              <p>last-login</p>
-            </div>
-          </a>
-          <hr/>
-          <a href="#page2">Role Request</a>
-          <hr/>
-          <a href="#page3">Student Admission Request</a>
-          <hr/>
-          <a href="#page4">Course Faculty Request</a>
-          <hr/>
-        </nav>
-        <main>
-          <div id="page2" className="page">
-            <h1>Role Request</h1>
-            <p>Role Request content</p>
-          </div>
-          <div id="page3" className="page">
-            <h1>Student Admission Request</h1>
-            <p>Student Admission Request page </p>
-          </div>
-          <div id="page4" className="page">
-            <h1>Course Faculty Request</h1>
-            <p>Course Faculty Request page</p>
-          </div>
-          <div id="page1" className="page default">
-            <h1>Admin page</h1>
-            <p>Admin Page</p>
-          </div>
-        </main>
+          <nav id="nav1">
+              <a href="#page1">
+                  <div>
+                      <img src="user.png" alt="" />
+                      <p>Admin name</p>
+                      <p> Role</p>
+                      <p>last-login</p>
+                  </div>
+              </a><hr />
+              <a href="#page2">Role Request</a><hr />
+              <a href="#page3">Student Admission Request</a><hr />
+              <a href="#page4">Course Faculty Request</a><hr />
+          </nav>
+          <main>
+              <div id="page2" className="page">
+                  <h1>Role Request</h1>
+                  <p>Role Request content</p>
+              </div>
+              <div id="page3" className="page">
+                  <h1>Student Admission Request</h1>
+                  <p>Student Admission Request page </p>
+              </div>
+              <div id="page4" className="page">
+                  <h1>Course Faculty Request</h1>
+                  <p>Course Faculty Request page</p>
+              </div>
+              <div id="page1" className="page default">
+                  <h1>Admin page</h1>
+                  <p>Admin Page</p>
+              </div>
+          </main>
       </div>
   );
 
